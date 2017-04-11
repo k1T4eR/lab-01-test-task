e7f13fbf73c775a30a9e48c644d194bbe7cbcc2b
k1T4eR <k1T4eR@gmail.com>
2017-04-11 19:04:41 +0300

    Explain autoload handler

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -3,10 +3,24 @@
 function missing_application_constant_handler($constant) {
     if ($constant === 'Application') {
         require_once __DIR__ . '/../Application.php';
+
+    // Example constant: Application\Routing\Route
     } else if (strpos($constant, 'Application') === 0) {
+
+        // Split by \: ['Application', 'Routing', 'Route']
         $tokens = explode('\\', $constant);
-        array_shift($tokens);
-        require_once __DIR__ . '/../' . join('/', $tokens) . '.php';
+
+        // Get targeted class: 'Route'
+        $target = array_pop($tokens); //
+
+        // Downcase the rest: ['application', 'routing']. We are following downcase directory naming.
+        $tokens = array_map(function($token) { return strtolower($token); }, $tokens);
+
+        // Put target class back to tokens: ['application', 'routing', 'Route']
+        array_push($tokens, $target);
+
+        // join('/', $tokens) . '.php' => application/routing/Route.php
+        require_once __DIR__ . '/../../' . join('/', $tokens) . '.php';
     }
 }
 
