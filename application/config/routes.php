179de3fbd426c09fe1cb7b36480c5e6d749f074c
k1T4eR <k1T4eR@gmail.com>
2017-04-12 17:07:44 +0300

    Drop ajax validation

--- application/config/routes.php
+++ application/config/routes.php
@@ -3,7 +3,6 @@
 use Application\Routing\Route;
 
 return [
-    new Route('index',              '/^\/$/',                        'GET',  'IndexController',          'index'),
-    new Route('newFeedbacksAPI',    '/^\/api\/feedbacks$/',          'POST', 'FeedbacksAPIController',   'create'),
-    new Route('emailValidationAPI', '/^\/api\/validations\/email$/', 'POST', 'ValidationsAPIController', 'validateEmail')
+    new Route('index',           '/^\/$/',               'GET',  'IndexController',        'index'),
+    new Route('newFeedbacksAPI', '/^\/api\/feedbacks$/', 'POST', 'FeedbacksAPIController', 'create')
 ];
